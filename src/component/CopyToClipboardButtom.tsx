aa83e4f2a174b83191519789857828e78a355159
Absebo <abseboco@gmail.com>
2026-01-25 16:45:11 +0100

    Give the copy buttom a little animation

--- src/component/CopyToClipboardButtom.tsx
+++ src/component/CopyToClipboardButtom.tsx
@@ -1,10 +1,13 @@
-import { JSX } from "solid-js";
+import { createSignal, JSX } from "solid-js";
 
 export function CopyToClipboardButton(
     props: JSX.ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
+    const [clipboardEffect, setClipboardEffect] = createSignal(false);
     function copyToClipboard() {
         navigator.clipboard.writeText(props.value ?? "");
+        setClipboardEffect(true);
+        setTimeout(() => setClipboardEffect(false), 200);
     }
     return (
         <button
@@ -13,9 +16,18 @@ export function CopyToClipboardButton(
                 "px-3 py-2.5 rounded-lg bg-indigo-600 shadow-sm hover:bg-indigo-700 hover:shadow-md active:bg-indigo-800 active:shadow-inner transition-all duration-200 " +
                 (props.class || "")
             }
-            onClick={(event) => copyToClipboard()}
+            onClick={() => copyToClipboard()}
         >
-            📋
+            <span>📋</span>
+            <span
+                style={{ opacity: clipboardEffect() ? 100 : 0 }}
+                class={
+                    "absolute transition-transform duration-200 right-3 bottom-2.5 " +
+                    (clipboardEffect() ? "transform-[translateY(-3rem)]" : "")
+                }
+            >
+                📋
+            </span>
         </button>
     );
 }
