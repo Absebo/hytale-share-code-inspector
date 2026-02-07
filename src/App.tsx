0e32582faba159100d31de7c0b8e40320944985b
Absebo <abseboco@gmail.com>
2026-02-07 18:25:47 +0100

    Add github link

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,24 @@ const App: Component = () => {
     return (
         <>
             <div class="flex justify-between items-center px-8 py-1">
-                <nav>
+                <nav class="flex gap-2">
                     <a
-                        class="px-2 pb-0.75 text-3xl rounded-lg bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 hover:shadow-md active:bg-indigo-800 active:shadow-inner transition-all duration-200"
+                        class="p-2 text-2xl rounded-lg bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 hover:shadow-md active:bg-indigo-800 active:shadow-inner transition-all duration-200"
                         href="/"
                     >
-                        ←
+                        🏠
+                    </a>
+                    <a
+                        class="p-2 rounded-lg bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 hover:shadow-md active:bg-indigo-800 active:shadow-inner transition-all duration-200"
+                        href="https://github.com/Absebo/hytale-share-code-inspector"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src="https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/github.svg"
+                            alt="GitHub project"
+                            style="width: 32px; height: 32px; filter: invert();"
+                        />
                     </a>
                 </nav>
                 <h1 class="text-4xl text-green-700 text-center py-2 mx-auto">
